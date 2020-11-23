88f70186c9d53bdcb20a308116ecef1b3a1a2a75
Nova <king-cz@outlook.com>
2020-11-23 18:10:47 +0800

    实现课程分类树

--- vue-admin-template-master/src/api/edu/subject.js
+++ vue-admin-template-master/src/api/edu/subject.js
@@ -1,5 +1,13 @@
 import request from '@/utils/request'
 
 export default {
+
+    // 获取课程分类的列表
+    getAllSubject(){
+        return request({
+            url:'/eduService/eduSubject/getSubject',
+            method:'get',
+        })
+    }
     
 }
--- vue-admin-template-master/src/views/edu/subject/subjectList.vue
+++ vue-admin-template-master/src/views/edu/subject/subjectList.vue
@@ -1,11 +1,62 @@
 <template>
-  <div id="app-container">
-    <h1>课程分类列表</h1>
+  <div class="app-container">
+    <el-input
+      v-model="filterText"
+      placeholder="Filter keyword"
+      style="margin-bottom: 30px"
+    />
+
+    <el-tree
+      ref="tree2"
+      :data="subjectList"
+      :props="defaultProps"
+      :filter-node-method="filterNode"
+      class="filter-tree"
+      default-expand-all
+    />
   </div>
 </template>
 
 <script>
-export default {};
+import subject from "@/api/edu/subject.js";
+
+export default {
+  data() {
+    return {
+      filterText: "",
+      subjectList: null,
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+  created() {
+    this.getAllSubject;
+  },
+  watch: {
+    filterText(val) {
+      this.$refs.tree2.filter(val);
+    },
+  },
+
+  methods: {
+    getAllSubject(){
+      subject.getAllSubject()
+      .then(response=>{
+        console.log(response);
+        // this.subjectList=response.data.SubjectList
+      })
+      .catch(error=>{
+        console.log(error);
+      })
+    },
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.label.indexOf(value) !== -1;
+    },
+  },
+};
 </script>
 
 <style>
